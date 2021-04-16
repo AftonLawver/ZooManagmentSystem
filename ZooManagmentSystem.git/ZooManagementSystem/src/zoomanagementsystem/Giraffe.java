a806a0a41b2e4b6ccacb4d7f7f854c560efd1e9c
faithenrietto <82626120+faithenrietto@users.noreply.github.com>
2021-04-16 10:15:48 -0500

    Add files via upload
    Test: changes made 4-16-21

---
+++ ZooManagmentSystem.git/ZooManagementSystem/src/zoomanagementsystem/Giraffe.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoomanagementsystem;
+
+/**
+ *
+ * @author Faith Enrietto
+ * 4-16-21
+ * Version 2
+ */
+public class Giraffe extends SuperAnimals{
+  
+    /**
+     * Giraffe constructor class
+     * @param enclosure
+     * @param name
+     * @param family
+     * @param animals
+     * @param food 
+     */
+    public Giraffe(int enclosure, String name, String family, int animals, int food){
+        //needs to extend the super class
+        super(enclosure, name, family, animals, food);  
+    }
+    
+    //getters?
+    
+}
