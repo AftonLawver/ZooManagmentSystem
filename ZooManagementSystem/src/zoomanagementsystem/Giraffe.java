c5ce4e27065a6d0835e35941f10f506469ac21b7
faithenrietto <fenrietto@gmail.com>
2021-04-15 22:50:01 -0500

    Added empty animal classes, need to add animal super class before amending individual animals

---
+++ ZooManagementSystem/src/zoomanagementsystem/Giraffe.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoomanagementsystem;
+
+/**
+ *
+ * @author fenri
+ */
+public class Giraffe {
+    private int enclosure;  //number of enclosure
+    private String name;    //animal name
+    private String family;  //type of animal (i.e. mammal, reptile..)
+    
+    public Giraffe(int enclosure, String name, String family){
+        //needs to extend the super class
+        //super(enclosure, name, family)
+        
+    }
+    
+    
+}
